15ac58207cdc6b9d136406b30e6e22b77be39f75
dnjoe96 <donjoedbest@gmail.com>
2021-08-18 18:22:13 -0700

    Create Shell Permission Scripts

--- easy.sh
+++ easy.sh
@@ -6,15 +6,17 @@ commandd=$2
 echo "$filename"
 
 echo "creating file"
-touch 0x00-shell_basics/$filename
+touch 0x01-shell_permissions/$filename
 
 echo "enter file information - $commandd"
-echo "#!/bin/bash" >> 0x00-shell_basics/$filename
-echo "$commandd" >> 0x00-shell_basics/$filename
+echo "#!/bin/bash" > 0x01-shell_permissions/$filename
+echo "$commandd" >> 0x01-shell_permissions/$filename
 
 echo "setting permissions"
-chmod u+x 0x00-shell_basics/$filename
+chmod u+x 0x01-shell_permissions/$filename
 
-echo "run script"
-./0x00-shell_basics/1-listit
+echo "DONE"
+#./0x00-shell_basics/1-listit
 
+echo 'wc -c' 
+tail -1 0x01-shell_permissions/$filename | wc -c
