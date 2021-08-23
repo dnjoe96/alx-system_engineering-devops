ba6e845c480364b8c36c5b8bbf3e12461d788ce0
dnjoe96 <donjoedbest@gmail.com>
2021-08-22 22:51:53 -0700

    Shell Redirection Script

--- easy.sh
+++ easy.sh
@@ -6,17 +6,17 @@ commandd=$2
 echo "$filename"
 
 echo "creating file"
-touch 0x01-shell_permissions/$filename
+touch 0x02-shell_redirections/$filename
 
 echo "enter file information - $commandd"
-echo "#!/bin/bash" > 0x01-shell_permissions/$filename
-echo "$commandd" >> 0x01-shell_permissions/$filename
+echo "#!/bin/bash" > 0x02-shell_redirections/$filename
+echo "$commandd" >> 0x02-shell_redirections/$filename
 
 echo "setting permissions"
-chmod u+x 0x01-shell_permissions/$filename
+chmod u+x 0x02-shell_redirections/$filename
 
 echo "DONE"
 #./0x00-shell_basics/1-listit
 
 echo 'wc -c' 
-tail -1 0x01-shell_permissions/$filename | wc -c
+tail -1 0x02-shell_redirections/$filename | wc -c
