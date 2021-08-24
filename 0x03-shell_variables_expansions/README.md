842f3b6dd4c4a972f8623c25a8fc44c3acd529ab
dnjoe96 <donjoedbest@gmail.com>
2021-08-24 09:44:29 -0700

    Shell variables

--- 0x03-shell_variables_expansions/README.md
+++ 0x03-shell_variables_expansions/README.md
@@ -1,1 +1,6 @@
 # shell variables
+ 
+`echo $PATH | tr -s ":" "\n"` --> to create a script that counts the number of directories in the PATH
+ 
+`echo {a..z}{a..z} | tr " " "\n" | grep -v "oo"` --> a script that prints all possible combinations of two letters except oo
+ 
--- easy.sh
+++ easy.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+if [ $1 == '--help' ]
+then
+	echo 'USAGE: ./easy.sh filename command usage'
+	exit
+
+elif [ $# -ne 3 ]
+then
+	echo 'Sorry Incomplete Paramters'
+        echo " "
+        exit
+else
+	echo 'processing file'
+fi
+
+
 filename=$1
 commandd=$2
+use=$3
 
 echo "$filename"
 
@@ -15,8 +31,17 @@ echo "$commandd" >> 0x03-shell_variables_expansions/$filename
 echo "setting permissions"
 chmod u+x 0x03-shell_variables_expansions/$filename
 
+echo "writing readme file"
+
+echo "\`$commandd\` --> $use" >> 0x03-shell_variables_expansions/README.md 
+echo " " >> 0x03-shell_variables_expansions/README.md
+
 echo "DONE"
 #./0x00-shell_basics/1-listit
 
-echo 'wc -l ' 
-tail -1 0x03-shell_variables_expansions/$filename | wc -c
+echo "wc -l\n" 
+wc -l 0x03-shell_variables_expansions/$filename
+
+# you can change the directory with the syntax
+# :%s/oldname/newname/    ENTER
+
