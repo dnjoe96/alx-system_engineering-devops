293a034af99483a000ad6c2091175ecaa25eb0d8
dnjoe96 <donjoedbest@gmail.com>
2022-05-02 10:34:28 +0100

    Comment file

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# Correct name spelling error in wp-settings.php file
 exec { 'modify_file':
     command => "sed -i 's|class-wp-locale.phpp|class-wp-locale.php|g' /var/www/html/wp-settings.php",
     path => '/bin',
